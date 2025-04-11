9cacdf32bc784e69ecc3b6ff05c054d8a21bedae
DjGoR <143566489+DjGoR@users.noreply.github.com>
2025-04-12 00:58:24 +0300

    Update video_downloader.js

--- video_downloader.js
+++ video_downloader.js
@@ -1,10 +1,10 @@
 (function(){
     if (!window.Plugin) return;
 
-    Plugin.register("video_downloader", {
-        title: 'Скачивание видео',
-        version: '1.0',
-        description: 'Добавляет кнопку для скачивания текущего видео.',
+    Plugin.register("video_downloader_ios", {
+        title: 'Скачивание видео (iOS совместимость)',
+        version: '1.1',
+        description: 'Добавляет кнопку для копирования ссылки на видео. Работает на iOS.',
         author: 'ChatGPT',
         type: 'video',
         onLoad: function() {
@@ -19,12 +19,16 @@
                         let btn = document.createElement('div');
                         btn.className = 'player-panel__button download-button';
                         btn.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M5 20h14v-2H5v2zm7-18L5.33 11h3.34v4h6.66v-4h3.34L12 2z"/></svg>';
-                        btn.title = 'Скачать видео';
+                        btn.title = 'Скопировать ссылку';
 
                         btn.addEventListener('click', () => {
                             let url = object.url;
                             if (url) {
-                                window.open(url, '_blank');
+                                navigator.clipboard.writeText(url).then(() => {
+                                    Lampa.Noty.show('Ссылка скопирована. Вставьте её в Safari или VLC');
+                                }).catch(() => {
+                                    Lampa.Noty.show('Ошибка копирования. Вот ссылка: ' + url);
+                                });
                             } else {
                                 Lampa.Noty.show('Ссылка для скачивания не найдена');
                             }
